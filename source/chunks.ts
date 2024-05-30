51f36ef4c43ab6ba1f4dd79abfcbaae4477b50fd
temich <tema.gurtovoy@gmail.com>
2024-05-30 15:33:06 +1000

    chore: cleanup

--- source/chunks.ts
+++ source/chunks.ts
@@ -7,13 +7,13 @@ export async function * chunks (response: Response): AsyncGenerator<string> {
   assert.ok(header !== null, 'Content-Type header is missing')
 
   const { type, parameters } = parse(header)
-  const boundary = parameters.boundary
-  const marker = `\r\n--${boundary}`
-  const cut = `${marker}\r\n`
-  const end = `${marker}--\r\n`
 
   assert.ok(type.startsWith('multipart/'), 'Content-Type is not multipart')
-  assert.ok(boundary !== undefined, 'Boundary parameter is missing')
+  assert.ok(parameters.boundary !== undefined, 'Boundary parameter is missing')
+
+  const marker = `\r\n--${parameters.boundary}`
+  const cut = `${marker}\r\n`
+  const end = `${marker}--\r\n`
 
   const reader = response.body!.getReader()
   const decoder = new TextDecoder()
@@ -34,10 +34,9 @@ export async function * chunks (response: Response): AsyncGenerator<string> {
 
     const parts = buffer.split(cut)
 
-    for (let i = 0; i < parts.length - 1; i++) {
+    for (let i = 0; i < parts.length - 1; i++)
       if (started) yield parts[i] // stream starts with boundary
       else started = true
-    }
 
     buffer = parts[parts.length - 1]
 
